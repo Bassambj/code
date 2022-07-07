be653ff427215d62c15681e6e9d99c6b32382f86
Bassambj <bassambj@gmail.com>
2022-07-06 23:45:57 -0300

    sqlyte x python

--- Stepy/Stepy.py
+++ Stepy/Stepy.py
@@ -1,20 +1,31 @@
 # Step Control - in python
 print("STEPY - STEP CONTROL IN PYTHON")
 import sqlite3
+conn = sqlite3.connect('Code\Stepy\Stepy.db')
+cursor = conn.cursor()
+print("Menu >\n1 - create table;\n2 - setup table;\n3 - print tables;")
+choice=int(input("Enter your choice: "))
 
-conn = sqlite3.connect('Stepy.db')
-
-# VERIFICAR SE A TABELA EXISTE SE NAO EXISTIR:
-table = conn.execute(
-    """SELECT step FROM Stepy.db
-""").fetchall
-
-# SE A TABELA NÃAO EXISTIR ELA É CRIADA
-if table == []:
+if(choice==1):
     conn.execute(''' CREATE TABLE step
-        (ID INT PRIMARY KEY NOT NULL,
-        DATE TEXT NOT NULL,
-        QUANTY INT NOT NULL,
-        ANNOTATION TEXT);''')
+    (DATE TEXT NOT NULL,
+    QUANTY INT NOT NULL,
+    ANNOTATION TEXT);''')
+elif(choice==2):
+    subChoice=0
+    while(subChoice!=1):
+        print("Insert step...")
+        date=input("Enter the date: ")
+        quanty=int(input("Enter the quant.: "))
+        annot=input("Enter any annotation: ")
+        cursor.execute(""" INSERT INTO step (DATE, QUANTY, ANNOTATION) VALUES (?,?,?) """, (date,quanty,annot))
+        conn.commit
+        subChoice=int(input("Type 1 to exit and 0 to inserto more steps. >"))
+elif(choice==3):
+    print("List of steps:")
+    conn.execute('SELECT * FROM step')
+else:
+    print("Error choice.")
+conn.close
 
 # UNDER CONSTRUTION

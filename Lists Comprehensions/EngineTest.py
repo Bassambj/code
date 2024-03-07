4464faa30908e82427ec0af1d94bb38ebb715b9e
Bassambj <bassambj@gmail.com>
2024-03-07 09:35:54 -0300

    BKP

--- Lists Comprehensions/EngineTest.py
+++ Lists Comprehensions/EngineTest.py
@@ -1,11 +1,15 @@
 # PEP 202 – List Comprehensions
 #https://peps.python.org/pep-0202/
 
-tank = 1000
+tank = 2050
 power = 0
 eficience = 3
-while (tank > 0):
-    [(power) for power in range(0,10) if (tank > 0) ]
+while (tank > 99):
+    [(power) for power in range(0,10) if (tank/100 > 0) ]
     tank = tank - 100
     power = (power+1) * eficience   
     print(power,tank)
+remmant = tank
+tank = tank - remmant
+power = (power+1) * eficience
+print(power,tank)
